2f4e75669a9a98c5e6af290cd360ec1b7e6c96cf
Roger <roger.kang.mo@gmail.com>
2015-11-26 19:12:50 -0800

    Initial commit tip calculator tip.ly ;)

--- tiply/ViewController.swift
+++ tiply/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let tipPercentages = [0.15, 0.20, 0.25]
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipSegment: UISegmentedControl!
+
+    func updateTotal() {
+        let selectedTipPercentage = tipPercentages[tipSegment.selectedSegmentIndex]
+        let amt = (billField.text! as NSString).doubleValue
+        let tipAmt = selectedTipPercentage * amt
+        let total = tipAmt + amt
+        
+        tipLabel.text = String(format: "$%.2f", tipAmt)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text =  "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTipPercentageChange(sender: AnyObject) {
+        updateTotal()
+    }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        updateTotal()
+    }
+
+    @IBAction func onScreenTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
